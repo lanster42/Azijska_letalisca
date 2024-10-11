372ed7742521c93cb7bb7e436ee1e7e764859551
Lan Stare <lan.stare42@gmail.com>
2024-10-11 21:01:58 +0200

    Delo s funkcijami

--- poberi.py
+++ poberi.py
@@ -2,51 +2,60 @@
 #importing
 
 import os
+import csv
 from bs4 import BeautifulSoup
 import requests
-import csv
 
 #####################################################################################
 
 print(os.getcwd())
 
+frontpage_url = "https://ourairports.com/continents/AS/airports.html"
 
-url = "https://ourairports.com/continents/AS/airports.html"
-page = requests.get(url)
-if page.status_code == 200:
-    # Parsiram HTML vsebino strani
-    doc = BeautifulSoup(page.content, "html.parser")
-
-
-#poisci linke vseh spletnih strani
-#poisci_next_link = doc.find(class_="nav col-sm-12").find(class_="next").find("a")["href"]
-#print(poisci_next_link)
-
+def dobi_permition(url):
+    page = requests.get(url, verify=False)
+    if page.status_code == 200:
+        # Parsiram HTML vsebino strani
+        doc = BeautifulSoup(page.content, "html.parser")
+        return doc
+    
+""" def save_string_to_file(text, directory, filename):
+    #Funkcija zapiše vrednost parametra "text" v novo ustvarjeno datoteko
+    #locirano v "directory"/"filename", ali povozi obstoječo. V primeru, da je
+    #niz "directory" prazen datoteko ustvari v trenutni mapi.
+    
+    os.makedirs(directory, exist_ok=True)
+    path = os.path.join(directory, filename) #dobimo polno pot 
+    with open(path, 'w', encoding='utf-8') as file_out:
+        file_out.write(text)
+    return None """
 
 #stevilo letalisc
-st_letalisc = int(doc.find(class_="badge hidden-sm").string.replace(",", ""))
-st_strani = st_letalisc//50 + 1
-print(st_strani)
+def stevilo_strani(url):
+    st_letalisc = int(dobi_permition(url).find(class_="badge hidden-sm").string.replace(",", ""))
+    st_strani = st_letalisc//50 + 1
+    return st_strani
 
-for page in range(st_strani + 1):
-    #Najprej pridobim permition za vsako stran posebaj (po straneh se lahko premikas le po 50 naenkrat)
-    url = f"https://ourairports.com/continents/AS/airports.html?start={page*50}"
-    page = requests.get(url)
-    if page.status_code == 200:
-    # Parsiram HTML vsebino strani
-        doc = BeautifulSoup(page.content, "html.parser")
+def pridobivanje_podatkov(url):
+    for page in range(stevilo_strani(url) + 1):
+        #Najprej pridobim permition za vsako stran posebaj (po straneh se lahko premikas le po 50 naenkrat)
+        url = f"{url}start={page*50}"
+        page = requests.get(url, verify=False)   #verify sem dodal, saj je strani med procesom potekel certifikat o varnosti :)
+        if page.status_code == 200:
+        # Parsiram HTML vsebino strani
+            doc = BeautifulSoup(page.content, "html.parser")
 
-    #Poišči ime letališča, kraj in drzavo
-    Slovar_letalisc = {}
-    ime_class = doc.find_all(class_="col-lg-6")
-    for letalisce in ime_class:
-        ime_letalisca = letalisce.find("h3").find("a").string
-        drzava = letalisce.find("p").find("b").string.strip()
-        tip_letalisca = letalisce.find("img")["title"]
-        Slovar_letalisc[ime_letalisca] = [drzava, tip_letalisca]
-    
-    #Slovarje hocem zapisati v csv datoteko
-    print(Slovar_letalisc)
+        #Poišči ime letališča, kraj in drzavo
+        Slovar_letalisc = {}
+        ime_class = doc.find_all(class_="col-lg-6")
+        for letalisce in ime_class:
+            ime_letalisca = letalisce.find("h3").find("a").string
+            drzava = letalisce.find("p").find("b").string.strip()
+            tip_letalisca = letalisce.find("img")["title"]
+            Slovar_letalisc[ime_letalisca] = [drzava, tip_letalisca]
+        
+        #Slovarje hocem zapisati v csv datoteko
+        print(Slovar_letalisc)
 
 
 #Zapiši podatke v csv datoteko
