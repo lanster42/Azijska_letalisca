07096686c6b2c05670a397085179242ef3f91ff4
Lan Stare <lan.stare42@gmail.com>
2024-10-13 11:57:42 +0200

    problem v readme in funkcija za url

--- README.md
+++ README.md
@@ -13,4 +13,7 @@ Za vsako Letališče bom zajel naslednje podatke:
 *glavna letališča bodo imela največ prihodov v zadnjih 4 urah
 
 ## Potek dela
-Najprej sem iz strani "https://ourairports.com/continents/AS/airports.html?start=100" pobral podatke v poberi.py. Te podatke sem nato shranil v novo datoteko
+Najprej sem iz strani "https://ourairports.com/continents/AS/airports.html?" pobral podatke v poberi.py. Te podatke sem nato shranil v novo datoteko
+
+## Zapleti in obrazložitve
+Šele naknadno sem ugotovil, da te vsak let pošlje na prehodno stran, na kateri je v obliki "widgeta" predstavljena vsebina o letih. Želel sem se vmesni strani izogniti tako, da bi najprej poiskal kodo letališča (ki je seveda edinstvena glede na letališče), to pa potem ustaviti v url končne strani z leti. To se na žalost ni obneslo, saj sem že pri prvem letališču (gl. letališče v Dubaiu) naletel na težavo: Nekatera letališča imajo več kot eno kodo. Na glavni strani je torej na primer za Dubai podana le koda "OMDB", končna spletna stran z leti pa ima v url drugo oznako: "DXB".
--- poberi.py
+++ poberi.py
@@ -2,7 +2,6 @@
 #importing
 from bs4 import BeautifulSoup
 import requests
-import re
 
 #####################################################################################
 import urllib3
@@ -11,24 +10,16 @@ from datetime import datetime, timedelta
 urllib3.disable_warnings()  #Stran je bila varna v prvih 2 tednih dela z njo in ji je nato potekel certifikat
 
 frontpage_url = "https://ourairports.com/continents/AS/airports.html"
-stran = requests.get(frontpage_url, verify=False)
-if stran.status_code == 200:
-    # Parsiram HTML vsebino strani
-    doc = BeautifulSoup(stran.content, "html.parser")
-    
-""" def save_string_to_file(text, directory, filename):
-    #Funkcija zapiše vrednost parametra "text" v novo ustvarjeno datoteko
-    #locirano v "directory"/"filename", ali povozi obstoječo. V primeru, da je
-    #niz "directory" prazen datoteko ustvari v trenutni mapi.
-    
-    os.makedirs(directory, exist_ok=True)
-    path = os.path.join(directory, filename) #dobimo polno pot 
-    with open(path, 'w', encoding='utf-8') as file_out:
-        file_out.write(text)
-    return None """
+
+def pridobi_potrdilo(zeljen_url):
+    potrdilo = requests.get(zeljen_url, verify=False) #verify sem dodal, saj je strani med pisanjem kode potekel certifikat o varnosti :)
+    if potrdilo.status_code == 200:
+    #Parsiram HTML vsebino vmesne strani
+        return BeautifulSoup(potrdilo.content, "html.parser")
 
 #stevilo letalisc
-def stevilo_strani():
+def stevilo_strani(zeljen_url):
+    doc = pridobi_potrdilo(zeljen_url)
     st_letalisc = int(doc.find(class_="badge hidden-sm").string.replace(",", ""))
     st_strani = st_letalisc//50 + 1
     return st_strani
@@ -74,30 +65,28 @@ def pridobi_lete_v_24urah(widget_url, local_date_time):
 
 def pridobivanje_podatkov():
     Seznam_slovarjev = []
-    for page in range(stevilo_strani() + 1):
+    for page in range(stevilo_strani(frontpage_url) + 1):
         #Najprej pridobim permition za vsako stran posebaj (po straneh se lahko premikas le po 50 naenkrat)
-        url = f"https://ourairports.com/continents/AS/airports.html?start={page*50}"
-        potrdilo1 = requests.get(url, verify=False)   #verify sem dodal, saj je strani med procesom potekel certifikat o varnosti :)
-        if potrdilo1.status_code == 200:
-        # Parsiram HTML vsebino strani
-            doc = BeautifulSoup(potrdilo1.content, "html.parser")
+        main_url = f"https://ourairports.com/continents/AS/airports.html?start={page*50}"
+        doc = pridobi_potrdilo(main_url)
 
-        #Poišči ime letališča, drzavo, tip letalisca in posebaj st. prihodov
+        #Poišči ime letališča, drzavo, tip letalisca, kodo letalisca in st. prihodov
         Sez_letalisc = []
         ime_class = doc.find_all(class_="col-lg-6")
         for letalisce in ime_class:
             ime_letalisca = letalisce.find("h3").find("a").string
             drzava = letalisce.find("p").find("b").string.strip()
             tip_letalisca = letalisce.find("img")["title"]
+            koda_letalisca = letalisce.find("h3").find("a")["href"].split('/')[2]
             
             # Lotimo se strani z odhodi in prihodi :')
-            link_prihodov = letalisce.find(title="Arrivals and departures")
-            # Preverim, ali link obstaja - btw to bi lah ful leps naredu ce ne bi sploh uporabu vmesne strani
-            if link_prihodov:
-                url_prihodov = f"https://ourairports.com{link_prihodov.get("href")}"  # Če obstaja, vzamem 'href'
+            poisci_prihode = letalisce.find(title="Arrivals and departures")
+            # Preverim, ali link obstaja
+            if poisci_prihode:
+                url_prihodov = f"https://ourairports.com{poisci_prihode.get("href")}"  # Če obstaja, vzamem 'href'
                 potrdilo2 = requests.get(url_prihodov, verify=False)
                 if potrdilo2.status_code == 200:
-                # Parsiram HTML vsebino strani
+                #Parsiram HTML vsebino vmesne strani
                     sec_doc = BeautifulSoup(potrdilo2.content, "html.parser")
                 print(url_prihodov)
                 #danes = datetime.date.today()
@@ -152,15 +141,12 @@ def pridobivanje_podatkov():
             else:
                 print("Element 'Arrivals and departures' ne obstaja")
 
-            Sez_letalisc.append({"ime letališča" : ime_letalisca, "država": drzava, "tip letališča" : tip_letalisca})
+            Sez_letalisc.append({"ime letališča" : ime_letalisca, "država": drzava, "tip letališča" : tip_letalisca, "koda letališča": koda_letalisca})
             #Slovar_letalisc[ime_letalisca] = [drzava, tip_letalisca]
         Seznam_slovarjev += (Sez_letalisc)
     return Seznam_slovarjev
 
 #TO DO:
-#  flights page(title="Arrivals and departures") --> how many a day
-#  sestavi funkcijo, ki uro in vrne stevilo prihodov zadnjih 24 ur(pazi na previous) sidenote: uposteval sem tudi unknown, scheduled in estimated
-#  zapisuj si tudi v koliko razlicnih mest letijo in letalsko druzbo. Lahko vzames tut delay
 
-#  Polepsaj kodo: namesto da gres na vsako stran posebaj iz inema letalisca vzemi href, ki vsebuje KRATICO.
-#  to kratico lahko nato ustavis v: https://www.avionio.com/widget/en/{KRATICA}/arrivals
+#  sestavi funkcijo, ki pogleda uro in vrne stevilo prihodov zadnjih 24 ur(pazi na previous) sidenote: uposteval sem tudi unknown, scheduled in estimated
+#  zapisuj si tudi v koliko razlicnih mest letijo in letalsko druzbo. Lahko vzames tut delay (ce si zelo ambiciozen)
