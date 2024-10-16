505032a7738a43f1a0fafef2b3917fd73aaf4e1e
Lan Stare <lan.stare42@gmail.com>
2024-10-16 11:26:35 +0200

    saving htmls

--- write_html.py
+++ write_html.py
@@ -2,18 +2,16 @@ import os
 import requests
 import poberi
 
-
+frontpage_url = "https://ourairports.com/continents/AS/airports.html"
 
 def shrani_html(frontpage_url):
-    os.makedirs("csv_datoteke", exist_ok=True)
+    os.makedirs("html_datoteke", exist_ok=True)
     
     for page in range(poberi.stevilo_strani(frontpage_url) + 1):
         # Najprej pridobim permition za vsako stran posebaj (po straneh se lahko premikas le po 50 naenkrat)
         main_urls = f"{frontpage_url}?start={page*50}"
-        
-        html = requests.get(main_urls)
-        
-        dat_ime = os.path.join("csv_datoteke", f"html_datoteka{page + 1}.html")
+        potrdilo = requests.get(main_urls, verify=False)  
+        dat_ime = os.path.join("html_datoteke", f"html_dat-{page + 1}.html")
 
         with open(dat_ime, "w", encoding="utf-8") as file:
-            file.write(html.text)
+            file.write(potrdilo.text)
